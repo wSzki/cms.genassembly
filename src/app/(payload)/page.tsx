b83eef6a881a26d5c65f7ca635501d15393283d8
wsz <59784520+wSzki@users.noreply.github.com>
2025-05-17 23:23:19 +0200

    remove routes, front end app

--- src/app/(payload)/page.tsx
+++ src/app/(payload)/page.tsx
@@ -1,11 +1,3 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
-
-import config from '@/payload.config'
-import './styles.css'
 import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
