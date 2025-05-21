bed512b3a3ac7d049124bec142deb59e2c6610d4
wsz <59784520+wSzki@users.noreply.github.com>
2025-05-22 00:46:42 +0200

    convert to webp

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,20 +1,68 @@
 import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  labels: {
-    singular: 'Image, Picture',
-    plural: 'Image, Pictures',
-  },
-  access: {
-    read: () => true,
-  },
-  fields: [
-    // {
-    //   name: 'alt',
-    //   type: 'text',
-    //   // required: true,
-    // },
-  ],
-  upload: true,
+    slug: 'media',
+    labels: {
+        singular: 'Image, Picture',
+        plural: 'Image, Pictures',
+    },
+    access: {
+        read: () => true,
+    },
+    fields: [
+        // {
+        //   name: 'alt',
+        //   type: 'text',
+        //   // required: true,
+        // },
+    ],
+    upload: {
+        staticDir: 'media',
+        formatOptions: {
+            format: 'webp',
+            options: {
+                quality: 80,
+            },
+        },
+        imageSizes: [
+            {
+                name: 'thumbnail',
+                width: 400,
+                height: undefined,
+                position: 'centre',
+                formatOptions: {
+                    format: 'webp',
+                    options: {
+                        quality: 80,
+                    },
+                },
+            },
+            // {
+            //     name: 'card',
+            //     width: 768,
+            //     height: undefined,
+            //     position: 'centre',
+            // },
+            // {
+            //     name: 'tablet',
+            //     width: 1024,
+            //     height: undefined,
+            //     position: 'centre',
+            // },
+            // {
+            //     name: 'desktop',
+            //     width: 1280,
+            //     height: undefined,
+            //     position: 'centre',
+            // },
+            // {
+            //     name: 'full',
+            //     width: 2048,
+            //     height: undefined,
+            //     position: 'centre',
+            // },
+        ],
+        adminThumbnail: 'thumbnail',
+        mimeTypes: ['image/*'],
+    },
 }
