28c76ac602da995a4f53c3fb70ddd652ad350218
wsz <59784520+wSzki@users.noreply.github.com>
2025-06-18 19:55:25 +0200

    corsz

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -26,6 +26,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+      cors: '*',
     admin: {
         user: Users.slug,
         importMap: {
