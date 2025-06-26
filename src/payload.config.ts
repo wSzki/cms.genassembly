a824cee8f0b0587ba5ae8f51a3ea92fb7188a1b1
wsz <59784520+wSzki@users.noreply.github.com>
2025-06-26 03:00:03 +0200

    retest

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -51,8 +51,8 @@ export default buildConfig({
         components: {
             // beforeLogin: ['/components/Icon'],
             beforeDashboard: ['/components/Logo'],
-            afterDashboard: ['./components/test.tsx'],
-            afterNavLinks: ['./components/nav.tsx'],
+            afterDashboard: ['/components/test.tsx'],
+            afterNavLinks: ['/components/nav.tsx'],
             graphics: {
                 Logo: '/components/Logo',
                 Icon: '/components/Icon',
